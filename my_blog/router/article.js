78c89cf296647a31a09b1a81a7fc6a885ff8a170
WangLinLin <WangLinLin@wll.com>
2018-11-07 23:54:13 +0800

    渲染发表文章页面的基本结构

--- my_blog/router/article.js
+++ my_blog/router/article.js
@@ -1,4 +1,6 @@
-// const express = require('express')
-// const router = express.Router()
-// const controller = require('../controller/article.js')
-// router.get('/article/add',controller.handleGetArticleAdd)
+const express = require('express')
+const router = express.Router()
+const controller = require('../controller/article.js')
+//监听客户端的get请求地址，显示文章添加页面
+router.get('/article/add',controller.handleGetArticleAdd)
+module.exports = router
